971d0c250cbb772945b37d4696315e0e1ea4ea4b
Risk of Rain <desmondtran000@gmail.com>
2025-02-09 00:39:28 +0700

    add folder structure, details in trello desc

---
+++ MNYT-BE/Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string AccountName { get; set; }
+        public Role Role { get; set; }
+    }
+}
