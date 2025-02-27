869e0d57a63dcdef603a956e56e611a1e2d09035
Risk of Rain <desmondtran000@gmail.com>
2025-02-27 21:09:07 +0700

    Update PregnancyVM

--- Application/ViewModels/Pregnancy/PregnancyVM.cs
+++ Application/ViewModels/Pregnancy/PregnancyVM.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Domain;
+using Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Application.ViewModels.Pregnancy
 {
-    public class PregnancyVM
+    public class PregnancyVM : BaseEntity
     {
         public int? AccountId { get; set; }
 
