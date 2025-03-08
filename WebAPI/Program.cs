9a8aea757921533fe311d275870b4502a7e37f2c
soloplayer345 <hongtlse172336@fpt.edu.vn>
2025-03-09 00:27:03 +0700

    Revert "Merge pull request #18 from SWP-SP25/feat/scheduleUser"
    This reverts commit dc2071bb2f81e82bc75e7e19650a5fcf6780cc19, reversing
    changes made to 109514ee6a9c4fddb5b6790e7fe2d56cd0ce6226.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -38,7 +38,7 @@ namespace WebAPI
 
             builder.Services.AddControllers();
 
-            builder.Services.AddInfrastructureServicesAsync(builder.Configuration).Wait();
+            builder.Services.AddInfrastructureServicesAsync(builder.Configuration);
             var schedulerService = builder.Services.BuildServiceProvider().GetRequiredService<IScheduleJobService>();
             // config Quartz 
             builder.Services.AddQuartz(q => schedulerService.ConfigureQuartz(q));
