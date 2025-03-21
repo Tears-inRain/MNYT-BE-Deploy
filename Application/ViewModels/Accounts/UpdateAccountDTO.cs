f7f33c216e08a1528025b656a42e4e058405a17b
soloplayer345 <hongtlse172336@fpt.edu.vn>
2025-03-21 20:17:44 +0700

    Update UpdateAccountDTO.cs

--- Application/ViewModels/Accounts/UpdateAccountDTO.cs
+++ Application/ViewModels/Accounts/UpdateAccountDTO.cs
@@ -10,5 +10,7 @@ namespace Application.ViewModels.Accounts
     {
         public string? FullName { get; set; }
         public string? PhoneNumber { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
     }
 }
