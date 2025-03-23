1e88ce4adee9e0a439619ca2ff5825c46176dab5
Risk of Rain <tuanthse151518@fpt.edu.vn>
2025-03-23 22:55:41 +0700

    allow anonymous blog post

---
+++ Infrastructure/Migrations/20250323155458_AnonymousBlogPost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AnonymousBlogPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAnonymous",
+                table: "BlogPosts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAnonymous",
+                table: "BlogPosts");
+        }
+    }
+}
