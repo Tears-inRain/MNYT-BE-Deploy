c05631d2c6d5e02326b0e13dd115796304911a89
Larry <90505134+soloplayer345@users.noreply.github.com>
2025-03-28 22:36:14 +0700

    Update VnPayService.cs
    change timezone again

--- Application/Services/VnPayService.cs
+++ Application/Services/VnPayService.cs
@@ -60,7 +60,7 @@ namespace Application.Services
             decimal amount = newMembership.Amount ?? 0;
             string orderDesc = $"Payment for membership plan ... (#{newMembership.Id})";
 
-            var timeZoneById = TimeZoneInfo.FindSystemTimeZoneById("Coordinated Universal Time");
+            var timeZoneById = TimeZoneInfo.FindSystemTimeZoneById("Greenwich Mean Time");
             var timeNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZoneById);
             var tick = DateTime.Now.Ticks.ToString();
 
